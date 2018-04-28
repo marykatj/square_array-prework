ffe579d6dc51b91fb7c984c4f8f7012ae74c9332
Mary-Kate James <marykatj@gmail.com>
2018-04-28 20:13:28 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_array = []
   array.each do |element|
-    square = element * element
+    square = element ** 2
     new_array.push(square)
     return new_array
   end
